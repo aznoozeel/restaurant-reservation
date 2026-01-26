7c1ddbf0eb571181deb790f9c77116f3ecdcf583
aznoozeel <for014768@gmail.com>
2026-01-26 15:59:43 +0900

    feat(adapter): CORS 설정 추가

--- api/src/main/kotlin/zoonza/restaurantreservation/adapter/in/security/config/SecurityConfiguration.kt
+++ api/src/main/kotlin/zoonza/restaurantreservation/adapter/in/security/config/SecurityConfiguration.kt
@@ -2,11 +2,15 @@ package zoonza.restaurantreservation.adapter.`in`.security.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import zoonza.restaurantreservation.adapter.`in`.security.jwt.JwtAuthenticationFilter
 import zoonza.restaurantreservation.adapter.`in`.security.jwt.JwtExceptionFilter
 import zoonza.restaurantreservation.adapter.`in`.security.oauth2.CustomOAuth2UserService
@@ -26,6 +30,7 @@ class SecurityConfiguration(
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
+            .cors { }
             .csrf { it.disable() }
             .httpBasic { it.disable() }
             .formLogin { it.disable() }
@@ -47,5 +52,19 @@ class SecurityConfiguration(
 //            }
 
         return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val config = CorsConfiguration().apply {
+            allowedOriginPatterns = listOf("http://localhost:5173")
+            allowedMethods = listOf("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            allowedHeaders = listOf("*")
+            allowCredentials = true
+        }
+
+        return UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**", config)
+        }
     }
 }
